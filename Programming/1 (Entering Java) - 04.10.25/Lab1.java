93afb135463b62e57ade83d139beb9ca5f967ff7
Your Name <you@example.com>
2025-10-10 16:56:18 +0300

    RENAME ALL

--- Programming/1 (Entering Java) - 04.10.25/Lab1.java
+++ Programming/1 (Entering Java) - 04.10.25/Lab1.java
@@ -1,12 +1,7 @@
-public class Lab1 {
+public class Lab1{
 
     public static void main(String[] args) {
-        short[] w = new short[16];
-        for (int i = 0; i < 16; i++) {
-            w[i] = (short) (i + 5);
-            System.out.print(w[i] + " ");   
-        }
-        System.out.println();
+        short[] w = {5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         double[] x = new double[20];
         for (int i = 0; i < x.length; i++) {
             x[i] = -15.0 + (12.0 - (-15.0)) * Math.random();
@@ -15,7 +10,7 @@ public class Lab1 {
         System.out.println();
         out(matrix(x, w, w.length, x.length));
     }
-    
+
     public static double[][] matrix(double[] masX, short[] masW ,int first, int second) {
         double[][] wx = new double[first][second];
         for (int i = 0; i < wx.length; i++) {
@@ -24,8 +19,8 @@ public class Lab1 {
                     case 7 -> wx[i][j] = Math.cos(Math.tan(Math.pow(masX[j], masX[j] * (masX[j] + 0.25))));
                     case 9, 10, 11, 13, 14, 15, 16, 18 -> wx[i][j] = Math.cos(Math.log(Math.abs(masX[j])));
                     default -> {
-                        double ground = 1 / (Math.pow(Math.E, 2 * ((Math.pow(2 * ((Math.abs(masX[j]) + 1) / Math.abs(masX[j])), 2)) + 1) / Math.PI));
-                        double sky = Math.pow(Math.atan(Math.pow(Math.E, -Math.abs(masX[j]))), (Math.pow(2 / (masX[j] - 1 / 3), masX[j] / 3)) + 1 / 2);
+                        double ground = 1 / (Math.exp(2 * ((Math.pow(((Math.abs(masX[j]) + 1) / Math.abs(masX[j])), 2)) + 1) / Math.PI / 2));
+                        double sky = Math.pow(Math.atan(Math.exp(-Math.abs(masX[j]))), (Math.cbrt(Math.pow(2 / (masX[j] - 1.0 / 3.0), masX[j]))) + 1.0 / 2.0);
                         wx[i][j] = Math.atan(Math.pow(ground, sky));
                     }
                 }
@@ -37,7 +32,7 @@ public class Lab1 {
     public static void out(double[][] wx) {
         for(double[] row: wx){
             for (double value: row){
-                System.out.print(String.format("%.3f", value) + " ");
+                System.out.printf("%7.3f ", value);
             }
             System.out.println(" ");
         }
