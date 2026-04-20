3824078a7a20059a31f250603c740ac95dbdba00
Gh0st1yAnge1 <yar-shald@yandex.ru>
2026-04-20 18:30:24 +0000

    replaced .docx reports with .pdf reports and removed dates

--- FPA/5 (Input\Outout devices)/task.asm
+++ FPA/5 (Input\Outout devices)/task.asm
@@ -1,39 +1,40 @@
 org 0x0593; начало программы
-      length: word 0x0007
-      buffer: word 0x0000
-      check: word 0x0001
-      symbols: word 0x0597
-      word 0xE5C4
-      word 0xE5F0
-      word 0xEEE2
-      word 0x003F
-      START: CLA
+length: word 0x0007
+buffer: word 0x0000
+flag: word 0x0000
+symbols: word 0x0597
+word 0xE5C4
+word 0xE5F0
+word 0xEEE2
+word 0x003F
+START: CLA; чисто символически обнуляем аккумулятор, ну а хули, собственно говоря
 
-      len: IN 7; читаем длину слова
-      AND #0x40
-      BEQ len; спин-луп
-      LD length
-      OUT 6
+len: IN 7; читаем длину слова (тупое условие, зачем выводить длину слова???)
+AND #0x40
+BEQ len; спин-луп
+LD length
+OUT 6
+BEQ exit; пидорасы, которые любят тыкать нули будут находиться в фрустрации
 
-      s1: IN 7; читаем первый символ
-      AND #0x40
-      BEQ s1; спин-луп
-      LD (symbols)+
-      ST buffer
-      OUT 6
+s: IN 7; читаем блядский символ 
+AND #0x40
+BEQ s; спин-луп
 
-      s2: IN 7; читаем второй символ
-      AND #0x40
-      BEQ s2; спин-луп
-      LD length;
-      CMP check;
-      BEQ loopJump;
-      LD buffer
-      SWAB
-      OUT 6
-      LD length
-      DEC
-      ST length
-      loopJump: LOOP length
-      JUMP s1
-      HLT
+LD flag
+BEQ loadNewCell
+LD buffer
+OUT 6
+LD #0x00
+ST flag
+JUMP loopJump
+
+loadNewCell: LD (symbols)+
+OUT 6
+SWAB
+ST buffer 
+LD #0x01
+ST flag
+
+loopJump: LOOP length
+JUMP s
+exit:HLT
